6caa96208ce09d0e3aa9ade9e11dba90b4c42b9d
Nijeesh <nijeesh@mallow-tech.com>
2020-02-26 13:57:02 +0530

    'changed CartItem.js' to a functional component

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
 import { List, Avatar } from 'antd';
 
-class CartItem extends Component {
-    constructor(props){
-        super(props)
-        this.item = props.item;        
-    }
-    
-    render() { 
+const CartItem = (props) =>{
         return (
             <List.Item style={{maxWidth: '400px'}}
-            actions={[ <span> Cost { this.item.bought * this.item.price } </span> ]}
+            actions={[ <span> Cost { props.item.bought * props.item.price } </span> ]}
             >
                 <List.Item.Meta
-                    avatar={<Avatar src={this.item.imageUrl} />}
-                    title={<a href="https://ant.design">{this.item.name}</a>}
-                    description={`Bought: ${this.item.bought} `}
+                    avatar={<Avatar src={props.item.imageUrl} />}
+                    title={<a href="https://ant.design">{props.item.name}</a>}
+                    description={`Bought: ${props.item.bought} `}
                     />
             </List.Item>
         );
-    }
 }
- 
+
 export default CartItem;
